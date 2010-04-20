96d054cbda01a480217623e73ac6355242e53e6f
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2010-04-20 01:01:12 -0300

    Changing strings to Unicode.

--- index.py
+++ index.py
@@ -17,41 +17,41 @@ class VideoResource(webapp.RequestHandler):
             pass #will return empty result :)
         
         if video:
-            self.response.out.write('<result>')
-            self.response.out.write('   <video>')
-            self.response.out.write('       <id>%s</id>' % video_id)
-            self.response.out.write('       <url><![CDATA[%s]]></url>' % video.media.player.url)
-            self.response.out.write('       <title><![CDATA[%s]]></title>' % video.title.text)
-            self.response.out.write('       <content><![CDATA[%s]]></content>' % video.content.text)
-            self.response.out.write('       <author><![CDATA[%s]]></author>' % video.author[0].name.text)
-            video.media.duration.seconds
-            self.response.out.write('       <comment_count>%d</comment_count>' % video.comments.feed_link[0].count_hint)
+            self.response.out.write(u'<result>')
+            self.response.out.write(u'   <video>')
+            self.response.out.write(u'       <id>%s</id>' % video_id)
+            self.response.out.write(u'       <url><![CDATA[%s]]></url>' % video.media.player.url)
+            self.response.out.write(u'       <title><![CDATA[%s]]></title>' % unicode(video.title.text))
+            self.response.out.write(u'       <content><![CDATA[%s]]></content>' % unicode(video.content.text))
+            self.response.out.write(u'       <author><![CDATA[%s]]></author>' % video.author[0].name.text)
+            self.response.out.write(u'       <duration>%d</duration>' % int(video.media.duration.seconds))
+            self.response.out.write(u'       <comment_count>%d</comment_count>' % int(video.comments.feed_link[0].count_hint))
             
-            self.response.out.write('       <categories>')
+            self.response.out.write(u'       <categories>')
             for category in [category.label for category in video.category if category.label]:
-                self.response.out.write('           <category><![CDATA[%s]]></category>' % category)
-            self.response.out.write('       </categories>')
+                self.response.out.write(u'           <category><![CDATA[%s]]></category>' % category)
+            self.response.out.write(u'       </categories>')
             
-            self.response.out.write('       <tags>')
+            self.response.out.write(u'       <tags>')
             if video.media.keywords.text:
-                for tag in video.media.keywords.text.split(','):
-                    self.response.out.write('           <tag><![CDATA[%s]]></tag>' % tag)
-            self.response.out.write('       </tags>')
+                for tag in video.media.keywords.text.split(u','):
+                    self.response.out.write(u'           <tag><![CDATA[%s]]></tag>' % tag.strip())
+            self.response.out.write(u'       </tags>')
             
-            self.response.out.write('       <thumbnails>')
+            self.response.out.write(u'       <thumbnails>')
             for thumbnail in video.media.thumbnail:
-                self.response.out.write('           <thumbnail width="%d" height="%d" time="">%s</thumbnail>' % (thumbnail.width, thumbnail.height, thumbnail.url, thumbnail.extension_attributes.get('time', '')))
-            self.response.out.write('       </thumbnails>')
+                self.response.out.write(u'           <thumbnail width="%d" height="%d" time="%s">%s</thumbnail>' % (int(thumbnail.width), int(thumbnail.height), thumbnail.extension_attributes.get('time', ''), thumbnail.url))
+            self.response.out.write(u'       </thumbnails>')
             
-            self.response.out.write('       <files>')
+            self.response.out.write(u'       <files>')
             for video_file in video.media.content:
-                self.response.out.write('           <file type="%s">%s</file>' % (video_file.type, video_file.url))
-            self.response.out.write('       </files>')
+                self.response.out.write(u'           <file type="%s"><![CDATA[%s]]></file>' % (video_file.type, video_file.url))
+            self.response.out.write(u'       </files>')
             
-            self.response.out.write('   </video>')
-            self.response.out.write('</result>')
+            self.response.out.write(u'   </video>')
+            self.response.out.write(u'</result>')
         else:
-            self.response.out.write('<result %s />' % video_id)
+            self.response.out.write(u'<result />')
 
 application = webapp.WSGIApplication(
                                      [('/video.*', VideoResource)],
